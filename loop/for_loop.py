5666be686ddcab8a0d7ec7a3991db99f9a5773ba
khalidhasan1102 <khalid.dc24@gmail.com>
2026-04-09 10:43:43 +0600

    ok

--- loop/for_loop.py
+++ loop/for_loop.py
@@ -1,3 +1,10 @@
+#for i in range(10):
+ #   print(i,("hello world"))
 
-for aa in  range(10):
-    print(aa)
+#for i in range (1,10):
+  #
+
+number = 2
+while number <20:
+    print("hello world")
+    number= number+5
